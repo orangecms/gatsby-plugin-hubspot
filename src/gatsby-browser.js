a6b69525e34bf93c9330a34ec9f72e8caf36e873
Daniel Maslowski <daniel.maslowski@img.ly>
2019-03-26 11:34:17 +0100

    only track with consent

--- src/gatsby-browser.js
+++ src/gatsby-browser.js
@@ -1,4 +1,21 @@
+import React from 'react'
+import {
+  registerTracker,
+  getTracker,
+} from 'gatsby-plugin-tracking-consent';
+
+const trackerId = 'hubspot';
+const trackerLabel = 'HubSpot';
+
+export function onClientEntry() {
+    registerTracker({ id: trackerId, label: trackerLabel });
+}
 export function onRouteUpdate({ location }) {
+    const tracker = getTracker(trackerId);
+    const consent = tracker && tracker.enabled;
+    if (!consent) {
+        return;
+    }
     if (process.env.NODE_ENV === 'production' && typeof _hsq === 'object') {
         let _hsq = window._hsq = window._hsq || [];
         _hsq.push(['setPath', location ? location.pathname + location.search + location.hash : undefined]);
